65b1e0cfd0e4e96c7c4cc436920fb9ca64128d61
Ian Esteves <pk20lian@gmail.com>
2025-11-14 00:08:34 -0300

    resultados

--- src/comparador.py
+++ src/comparador.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 comparador.py
 
@@ -33,7 +32,7 @@ def find_target_dirs(root: str = ".") -> dict:
 
     Retorna um dicionário {folder_name: [path1, path2, ...]}.
     """
-    names = ["mcfv-results", "mcev-results", "qlrn-results"]
+    names = ["mcfv-results", "mcev-results"] #"qlrn-results"
     found = {n: [] for n in names}
 
     # procurar no root e em root/results
